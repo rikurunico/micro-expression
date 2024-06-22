6516bf54ff50146db511b8628dc2b4e9019625a0
rikurunico <rikurunico@gmail.com>
2024-06-22 14:26:31 +0700

    Refactor KNNClassifier class for improved readability and maintainability

--- clasification/knn.py
+++ clasification/knn.py
@@ -1,26 +1,17 @@
 import os
 import pandas as pd
-from sklearn.model_selection import (
-    cross_val_score,
-    KFold,
-    train_test_split,
-    GridSearchCV,
-)
+from sklearn.model_selection import cross_val_score, KFold, train_test_split, GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 import joblib
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 class KNNClassifier:
-    def __init__(
-        self,
-        dataset_file,
-        label_column,
-        feature_column=None,
-        except_feature_column=None,
-    ):
+    def __init__(self, dataset_file, label_column, feature_column=None, except_feature_column=None):
         self.dataset_file = dataset_file
         self.feature_column = feature_column
         self.except_feature_column = except_feature_column
@@ -38,16 +29,10 @@ class KNNClassifier:
         data = pd.read_csv(self.dataset_file)
         print(f"Loaded dataset from {self.dataset_file}\n")
 
-        if self.feature_column is None and (
-            self.except_feature_column is None or self.except_feature_column == [None]
-        ):
-            raise ValueError(
-                "The 'feature_column' and 'except_feature_column' parameters are both empty. One of them must be provided."
-            )
+        if self.feature_column is None and (self.except_feature_column is None or self.except_feature_column == [None]):
+            raise ValueError("The 'feature_column' and 'except_feature_column' parameters are both empty. One of them must be provided.")
 
-        if self.except_feature_column is not None and self.except_feature_column != [
-            None
-        ]:
+        if self.except_feature_column is not None and self.except_feature_column != [None]:
             self.X = data.drop(self.except_feature_column, axis=1).values
         elif self.feature_column is not None:
             self.X = data[self.feature_column].values
@@ -55,9 +40,7 @@ class KNNClassifier:
         self.y = self.label_encoder.fit_transform(data[self.label_column].values)
 
     def split_data(self, test_size=0.2, random_state=0):
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
-            self.X, self.y, test_size=test_size, random_state=random_state
-        )
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=test_size, random_state=random_state)
 
         # Feature scaling
         scaler = StandardScaler()
@@ -75,9 +58,7 @@ class KNNClassifier:
             # gunakan KFold dengan k=10
             cv = KFold(n_splits=10, shuffle=True, random_state=0)
 
-            grid_search = GridSearchCV(
-                KNeighborsClassifier(), param_grid, cv=cv, n_jobs=-1
-            )
+            grid_search = GridSearchCV(KNeighborsClassifier(), param_grid, cv=cv, n_jobs=-1)
 
             grid_search.fit(self.X_train, self.y_train)
             print(f"Best hyperparameters: {grid_search.best_params_}")
@@ -85,9 +66,7 @@ class KNNClassifier:
         else:
             print("Training model with specified parameters:")
             print(f"n_neighbors: {n_neighbors}, metric: {metric}, p: {p}")
-            self.model = KNeighborsClassifier(
-                n_neighbors=n_neighbors, metric=metric, p=p
-            )
+            self.model = KNeighborsClassifier(n_neighbors=n_neighbors, metric=metric, p=p)
             self.model.fit(self.X_train, self.y_train)
 
     def evaluate_model(self):
@@ -95,28 +74,27 @@ class KNNClassifier:
         accuracy = accuracy_score(self.y_test, predictions)
         cm = confusion_matrix(self.y_test, predictions)
 
+        # Display the confusion matrix with proper formatting
+        plt.figure(figsize=(10, 7))
+        sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=self.label_encoder.classes_, yticklabels=self.label_encoder.classes_)
+        plt.xlabel("Predicted labels")
+        plt.ylabel("True labels")
+        plt.title("Confusion Matrix")
+        plt.show()
+
         print("\nModel Evaluation:")
         print(f"Test Accuracy: {accuracy:.4f}")
         print("\nConfusion Matrix:")
         print(cm)
 
-    # def save_model(self, filename="knn_model.joblib"):
-    #     output_model_path = "models"
-    #     if not os.path.exists(output_model_path):
-    #         os.makedirs(output_model_path)
-    #     joblib.dump(self.model, os.path.join(output_model_path, filename))
-    #     print(f"Model saved to {os.path.join(output_model_path, filename)}")
-
-    def save_model(
-        self, filename="svm_model.joblib", label_encoder_filename="label_encoder.joblib"
-    ):
+    def save_model(self, filename="knn_model.joblib", label_encoder_filename="label_encoder.joblib"):
         output_model_path = "models"
         if not os.path.exists(output_model_path):
             os.makedirs(output_model_path)
         joblib.dump(self.model, os.path.join(output_model_path, filename))
-        joblib.dump(
-            self.label_encoder, os.path.join(output_model_path, label_encoder_filename)
-        )
+        joblib.dump(self.label_encoder, os.path.join(output_model_path, label_encoder_filename))
+        print(f"Model saved to {os.path.join(output_model_path, filename)}")
+        print(f"Label encoder saved to {os.path.join(output_model_path, label_encoder_filename)}")
 
 
 # Example usage:
